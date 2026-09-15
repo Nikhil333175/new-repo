a188f96ea1ff69bfcb0ab6dba597319198ce1137
Nikhil Murali <nikhilmurali175@gmail.com>
2026-09-15 12:16:44 +0530

    Create App.java with main method and print statements

---
+++ App.java
@@ -0,0 +1,10 @@
+public class App {
+
+```
+public static void main(String[] args) {
+    System.out.println("Hello, Jenkins!");
+    System.out.println("This is a sample Java Maven application.");
+}
+```
+
+}
